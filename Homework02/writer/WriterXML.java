5870452d8e3ac2d72fbb2b95911d7d7a799527f9
Margarita <margarita27011991@gmail.com>
2022-12-19 23:25:04 +0300

    add Homework02

--- Homework02/writer/WriterXML.java
+++ Homework02/writer/WriterXML.java
@@ -8,14 +8,14 @@ public class WriterXML extends WriteTo{
     @Override
     public void FileWriter(Map<String, Double> args) {
         try(FileWriter writer = new FileWriter("Homework02//formatXML.xml", false)) {
-            writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n");
+            writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<students>\n");
             for(Map.Entry<String, Double> entry: args.entrySet()) {
                 writer.append("  <Student>\n");
                 writer.append("    <name>" + entry.getKey() + "</name>\n");
                 writer.append("    <averagemark>" + entry.getValue() + "</averagemark>\n");
                 writer.append("  </Student>\n");
             }
-            writer.append("</root>\n");
+            writer.append("</students>\n");
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         } 
